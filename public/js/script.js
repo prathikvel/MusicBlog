3c79967c562a052cc4a42c50b5502f6f0347ad84
Prathik Velagapudi <velagapudi_prathik@hotmail.com>
2020-06-17 20:22:19 -0400

    remove tagline and typedjs

--- public/js/script.js
+++
@@ -1,9 +0,0 @@
-const typed = new Typed('#typed', {
-    strings: ["One stop for all things music"],
-    typeSpeed: 30,
-    backSpeed: 40,
-    startDelay: 200,
-    smartBackspace: true,
-    loop: false,
-    showCursor: false
-});
