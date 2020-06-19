c8e3ebbe2b3674a0e70c4457b8a42d92b281c625
Prathik Velagapudi <velagapudi_prathik@hotmail.com>
2020-06-19 15:26:04 -0400

    modularize css

--- index.js
+++ index.js
@@ -20,25 +20,27 @@ seedDB();
 
 // MIDDLEWARE
 function localVariables (req, res, next) {
-    res.locals.path = req.path;
-    next();
+    res.locals.moment = moment;
+    next()
 }
 app.use(localVariables);
 
 // ROUTES
 app.get("/", async (req, res) => {
-    let posts = await Models.Post.find({});
-    let features = [
+    const posts = await Models.Post.find({});
+    const features = [
         await Models.Post.findOne({"feature.main": true}),
         await Models.Post.findOne({"feature.side-a": true}),
         await Models.Post.findOne({"feature.side-b": true})
     ];
-    res.render("home", {posts: posts, features: features, moment: moment});
+    const view = "home";
+    res.render(view, {view: view, posts: posts, features: features});
 });
 app.get("/blog/:id", async (req, res) => {
-    let post = await Models.Post.findById(req.params.id);
-    let stripBackgroundColor = await colorThief.getColor(post.image);
-    res.render("show", {post: post, stripBackgroundColor: stripBackgroundColor});
+    const post = await Models.Post.findById(req.params.id);
+    const stripBackgroundColor = await colorThief.getColor(post.image);
+    const view = "show";
+    res.render("show", {view: view, post: post, stripBackgroundColor: stripBackgroundColor});
 });
 
 // SERVER
