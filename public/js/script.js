92c6013473e7055ab34ac2ec01a041a86a02e079
Prathik Velagapudi <velagapudi_prathik@hotmail.com>
2020-06-19 04:27:56 -0400

    overhaul scss and design show page

--- public/js/script.js
+++ public/js/script.js
@@ -10,8 +10,15 @@ function nav () {
     }
 }
 
+function offsetShowImage () {
+    const showImage = $(".show .layout img");
+    showImage.on("load", () => showImage.css("marginBottom", `-${showImage.height() / 3}px`));
+    $(window).resize(() => showImage.css("marginBottom", `-${showImage.height() / 3}px`));
+}
+
 function init () {
     nav();
+    offsetShowImage();
 }
 
 $(init());
